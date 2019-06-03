2e10834f8370d6b2d7df3ecf5bdd8314fb70ce9c
DESKTOP-82ANFV3\thesi <perezquimey@gmail.com>
2019-06-03 15:55:05 -0300

    TestCompleto casi terminado, falta la factura que lo estoy desarrollando la idea aun, agrege metodos nuevos en Funciones para facilitar el examen

--- Programacion/src/negocio/Funciones.java
+++ Programacion/src/negocio/Funciones.java
@@ -36,6 +36,101 @@ public class Funciones {
 		return hora.getSecond();
 	}
 	
+	//-----------------------------------------------------------------------
+	
+	public static boolean esBisiesto(int anio) {
+		boolean resultado = false;
+		if (anio % 4 == 0 && ((anio % 100 != 0) || (anio % 400 == 0))) {
+			resultado = true;
+		}
+		return resultado;
+	}
+
+	public static boolean esFechaValida(int anio, int mes, int dia) {
+		boolean resultado = true;
+
+		if (mes < 1 || mes > 12) {
+			resultado = false;
+		}
+
+		if (anio < 1) {
+			resultado = false;
+		}
+
+		switch (mes) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12: {
+			if (dia < 1 || dia > 31) {
+				resultado = false;
+			}
+			break;
+		}
+
+		case 2: {
+			if (esBisiesto(anio) == true) {
+				if (dia < 1 || dia > 29) {
+					resultado = false;
+				}
+			} else {
+				if (dia < 1 || dia > 28) {
+					resultado = false;
+				}
+			}
+			break;
+		}
+		case 4:
+		case 6:
+		case 9:
+		case 11: {
+			if (dia < 1 || dia > 30) {
+				resultado = false;
+			}
+			break;
+		}	
+		default:
+			resultado = false;
+			break;
+		}
+
+		return resultado;
+	}
+	
+	//-----------------------------------------------------------------------
+	
+	public static LocalTime traerTiempo(int hour, int minute, int second) {
+		LocalTime LT = LocalTime.of(hour, minute, second);
+		return LT;
+	}
+	
+	public static LocalDate traerFecha(String fecha) {
+		// 20/03/2018 Primero el dia, despues mes y despues año
+		
+		return traerFecha(Integer.parseInt(fecha.substring(0, 2)), Integer.parseInt(fecha.substring(3, 5)),
+						Integer.parseInt(fecha.substring(6, 10)));
+	}
+	
+	public static LocalDate traerFecha(int dia, int mes, int anio) {
+		LocalDate LD = null;
+		if (esFechaValida(anio, mes, dia))
+		LD = LocalDate.of(anio, mes, dia);
+		return LD;
+	}
+	
+	public static LocalDate traerFechaAhora() {
+		return LocalDate.now();
+	}
+	
+	public static LocalTime traerTiempoAhora() {
+		return LocalTime.now();
+	}
+	//-----------------------------------------------------------------------
+	
+
 	public static String traerFechaCorta(LocalDate fecha) {
 		return traerDia(fecha) + "/" + traerMes(fecha) + "/" + traerAnio(fecha);
 	}
--- Programacion/src/test/TestCompleto.java
+++ Programacion/src/test/TestCompleto.java
@@ -1,10 +1,15 @@
 package test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import dao.TarifaDao;
 import datos.Cliente;
 import datos.DatosPersonales;
+import datos.Factura;
 import datos.Inspector;
+import datos.Lectura;
+import datos.LecturaBajaDemanda;
 import datos.Medidor;
 import datos.PersonaFisica;
 import datos.PersonaJuridica;
@@ -14,6 +19,7 @@ import datos.Zona;
 import negocio.ClienteABM;
 import negocio.DatosPersonalesABM;
 import negocio.DetallesTarifaABM;
+import negocio.Funciones;
 import negocio.InspectorABM;
 import negocio.LecturaABM;
 import negocio.MedidorABM;
@@ -23,81 +29,160 @@ import negocio.ZonaABM;
 public class TestCompleto {
 
 	public static void main(String[] args) {
-		
+
 		DatosPersonalesABM abm = DatosPersonalesABM.getInstancia();
 		ClienteABM clienteABM = ClienteABM.getInstancia();
 		InspectorABM inspectorABM = InspectorABM.getInstancia();
 		ZonaABM zabm = ZonaABM.getInstacia();
-		TarifaABM tarifaABM = TarifaABM.getInstancia();
+		TarifaABM TABM = TarifaABM.getInstancia();
 		MedidorABM medidorABM = MedidorABM.getInstancia();
 		DetallesTarifaABM DTABM = DetallesTarifaABM.getInstancia();
 		LecturaABM LABM = LecturaABM.getInstaciaABM();
-		
-		//--------------------------------------------------------------------- Agrego Una persona Fisica
-		DatosPersonales DP = new DatosPersonales("Quimey","Perez",41237009);
+
+		// ESTO SOLAMENTE ES PARA UN CLIENTE DE BAJA
+
+		// ----------------------------------------------------------------------
+		// Agregar TarifaBaja con sus detalles por las dudas
+
+		try {
+			TABM.agregarTarifaBaja("T1 - R1");
+			TABM.agregarTarifaBaja("T1 - R2");
+			TABM.agregarTarifaBaja("T1 - R3");
+			TABM.agregarTarifaBaja("T1 - R4");
+			TABM.agregarTarifaBaja("T1 - R5");
+			TABM.agregarTarifaBaja("T1 - R6");
+			TABM.agregarTarifaBaja("T1 - R7");
+			TABM.agregarTarifaBaja("T1 - R8");
+			TABM.agregarTarifaBaja("T1 - R9");
+
+			// DetalleBaja(String detalleConceptos,String unidad, int desde, int hasta,
+			// double valor, TarifaBaja tarifaBaja)
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 0, 150, 32.82, TABM.traerTarifaBaja("T1 - R1"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 0, 150, 2.653, TABM.traerTarifaBaja("T1 - R1"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 151, 325, 56.11, TABM.traerTarifaBaja("T1 - R2"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 151, 325, 2.651, TABM.traerTarifaBaja("T1 - R2"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 326, 400, 91.73, TABM.traerTarifaBaja("T1 - R3"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 326, 400, 2.705, TABM.traerTarifaBaja("T1 - R3"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 401, 450, 107.34, TABM.traerTarifaBaja("T1 - R4"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 401, 450, 2.785, TABM.traerTarifaBaja("T1 - R4"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 451, 500, 161.66, TABM.traerTarifaBaja("T1 - R5"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 451, 500, 2.864, TABM.traerTarifaBaja("T1 - R5"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 501, 600, 316.00, TABM.traerTarifaBaja("T1 - R6"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 501, 600, 2.918, TABM.traerTarifaBaja("T1 - R6"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 601, 700, 850.24, TABM.traerTarifaBaja("T1 - R7"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 601, 700, 3.309, TABM.traerTarifaBaja("T1 - R7"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 701, 1400, 1203.64, TABM.traerTarifaBaja("T1 - R8"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 701, 1400, 3.411, TABM.traerTarifaBaja("T1 - R8"));
+
+			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 1401, 9999, 1442.88, TABM.traerTarifaBaja("T1 - R9"));
+			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 1401, 9999, 3.437, TABM.traerTarifaBaja("T1 - R9"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+		// --------------------------------------------------------------------- Agrego
+		// Una persona Fisica
+		DatosPersonales DP = new DatosPersonales("Quimey", "Perez", 41237009);
 		PersonaFisica PF = new PersonaFisica("9 de basto 3000", DP);
 		try {
 			System.out.println(abm.agregarDatosPersonales(DP));
 			System.out.println(clienteABM.agregarPersonaFisica(PF));
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
-		//--------------------------------------------------------------------- Agrego inspector
-				
-		DatosPersonales DP2 = new DatosPersonales("Juan","Domingez",42347332);
-		Inspector inspector1 = new Inspector(DP2,"20-42347332-1");
-		try {
-			System.out.println(abm.agregarDatosPersonales(DP2));
-			System.out.println(inspectorABM.agregarInspector(inspector1));
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-		
-		//---------------------------------------------------------------------- Agrego una Zona (Lanús)
-		
+
+		// ---------------------------------------------------------------------- Agrego
+		// una Zona (Lanús)
+
 		Zona z = new Zona("Lanús");
 		try {
 			System.out.println(zabm.agregarZona(z));
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
-		
-		//---------------------------------------------------------------------- Agregar Tarifa de Cliente Quimey
-			
+
+		// --------------------------------------------------------------------- Agrego
+		// inspector
+
+		DatosPersonales DP2 = new DatosPersonales("Juan", "Domingez", 42347332);
+		Inspector inspector1 = new Inspector(DP2, "20-42347332-1");
 		try {
-			tarifaABM.agregarTarifaBaja("T1 - R1");
-			//DetalleBaja(String detalleConceptos,String unidad, int desde, int hasta, double valor, TarifaBaja tarifaBaja)
-			DTABM.agregarDetalleBaja("Cargo Fijo", "$/mes", 0, 150, 32.82, tarifaABM.traerTarifaBaja("T1 - R1"));
-			DTABM.agregarDetalleBaja("Cargo Variable", "$/kWh", 0, 150, 2.653, tarifaABM.traerTarifaBaja("T1 - R1"));
+			System.out.println(abm.agregarDatosPersonales(DP2));
+			System.out.println(inspectorABM.agregarInspector(inspector1));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+		
+		// --------------------------------------------------------------------- Agrego Intermedio
+		// inspector has zonas
+		
+		try {
+			inspectorABM.agregarInspectorEnZona(z, inspector1); //se hace despues de agregar los dos
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
 		
-		//---------------------------------------------------------------------- Agregar Medidor de Cliente Quimey
-		//Medidor(String direccion, Zona zona,Tarifa tarifa,Cliente cliente)
+		
+		// ----------------------------------------------------------------------
+		// Agregar Medidor de Cliente Quimey
+		// Medidor(String direccion, Zona zona,Tarifa tarifa,Cliente cliente)
 
 		try {
-			Tarifa T = tarifaABM.traerTarifaBaja("T1 - R1"); //Traigo una tarifa, en este caso por ser fisico corresponde a la baja
-			Medidor medidor1 = new Medidor(PF.getDireccion(), z, T, PF ); 
+			Tarifa T = TABM.traerTarifaBaja("T1 - R1"); // Traigo una tarifa, en este caso por ser fisico corresponde a
+														// la baja
+
+			// Medidor(String direccion, Zona zona,Tarifa tarifa,Cliente cliente)
+			Medidor medidor1 = new Medidor(PF.getDireccion(), z, T, PF);
 			System.out.println(medidorABM.agregarMedidor(medidor1));
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
+
+		// ----------------------------------------------------------------------
+		// Agregar lectura de medidor
+		// LecturaBajaDemanda(LocalTime hora, LocalDate fecha, Inspector inspector,
+		// Medidor medidor,double energiaConsumida)
+
+		try {
+			LecturaBajaDemanda L1 = new LecturaBajaDemanda(Funciones.traerTiempo(15, 0, 0),
+					Funciones.traerFecha("03/02/2019"), inspectorABM.traerInspectorDni(42347332),
+					medidorABM.traerMedidor(PF.getDireccion()), 100.0);
+
+			LecturaBajaDemanda L2 = new LecturaBajaDemanda(Funciones.traerTiempo(15, 0, 0),
+					Funciones.traerFecha(3, 4, 2019), inspectorABM.traerInspectorDni(42347332),
+					medidorABM.traerMedidor(PF.getDireccion()), 75.0);
+
+			System.out.println(LABM.agregarLectura(L1));
+			System.out.println(LABM.agregarLectura(L2));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		// ----------------------------------------------------------------------
+		// Agregar Factura(int nroSerieMedidor, int idCliente, LocalDate fecha, String observaciones)
+		//Fecha es cuando se emitio la boleta, que debe ser un mes despues de la ultima
+		//observaciones Va la lectura anterior con la lectura ultima concatenada en una string
 		
-		//---------------------------------------------------------------------- Agregar lectura  de medidor
-		
-		
-		
-		//---------------------------------------------------------------------- Agregar Facturas
-		
-		
+		//ItemFactura(String detalle, double precioUnitario, int cantidad, String unidad, Factura factura)
+		/*detalle;		Si el consumo es Bajo o Alto
+		precioUnitario;	Esta en Tarifa los valores que corresponderan si la lectura
+		cantidad;		La energiaConsumida es la suma entre lecturaBaja/Alta anterior y lecturaBaja/Alta ultima
+		unidad;			$
+		*/
 		
 		
 		
